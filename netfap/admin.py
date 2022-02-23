6df1a834f55442ee535f6f58e0cb781d224e7f29
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-02-24 04:18:52 +0500

    add adminpanel

--- netfap/admin.py
+++ netfap/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
+from django.contrib import admin
+from .models import Actors,Movie
 # Register your models here.
+
+@admin.register(Movie)
+
+class MoveiAdmin(admin.ModelAdmin):
+    list_display=['name','year','imdb']
+
+@admin.register(Actors)
+class ActorsAdmin(admin.ModelAdmin):
+    
+    list_display = ['name','birth_date',] 
+   
--- netfap/models.py
+++ netfap/models.py
@@ -6,20 +6,29 @@ GENDER_CHOICES = [
      ('W','WOMEN'),
 ]
 GENRE = [
-    ('',''),
+    ('M','Melodramma'),
+    ('R','Romantic'),
+    ('C','Comedy'),
+    ('D','Dramma'),
+    ('F','Fantasy')
     
 ]
 class Movie(models.Model):
     name = models.CharField(max_length=150)
     year = models.DateField(auto_now_add=True)
-    imdb = models.models.IntegerField()
-    genre = models.models.CharField(max_length=1)
+    imdb = models.IntegerField()
+    genre = models.CharField(choices=GENRE,max_length=1)
+    cust = models.ManyToManyField('Actors')
     
-       
+    def __str__(self):
+        return self.name
     
-class Actor(models.Model):
+class Actors(models.Model):
     
     name = models.CharField(max_length=150)
     birth_date = models.DateField()
     gender = models.CharField(choices=GENDER_CHOICES,max_length=1)
+    films = models.ManyToManyField(Movie)
+    def __str__(self):
+        return self.name
         
